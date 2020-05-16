3475f814761d5f7d4c3427238079da5271474da4
unknown <2954588290@qq.com>
2020-05-16 20:30:30 +0800

    Hello my first repository

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a;
+	int b[10]={1,2,3,4,5}; 
+	cout<< "hello world£¡"<<endl ;
+	
+	for(a=0;a<10;a++)
+	cout<<b[a]<<endl;
+	return 0;
+}
